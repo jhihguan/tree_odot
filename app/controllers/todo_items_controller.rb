0da5f50d5f467210472e9d0808b6416da58b57bb
jhihguan <imdiedie@gmail.com>
2014-08-13 02:10:00 -0700

    add editing but no update controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -21,6 +21,16 @@ class TodoItemsController < ApplicationController
     end
   end
 
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+
+  end
+
+  def url_options
+    { todo_list_id: params[:todo_list_id]}.merge(super)
+  end
+
   private
 
   def todo_item_params
